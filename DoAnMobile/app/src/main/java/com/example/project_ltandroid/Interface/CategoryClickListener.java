decb2064853a67521ef3d87a35e83f3c5168d986
Linh2404XD <147931651+Linh2404XD@users.noreply.github.com>
2024-06-26 21:14:05 +0700

    Delete DoAnMobile/app/src/main/java/com/example/project_ltandroid directory

--- DoAnMobile/app/src/main/java/com/example/project_ltandroid/Interface/CategoryClickListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.project_ltandroid.Interface;
-
-import android.view.View;
-
-public interface CategoryClickListener {
-    void onClick(View view, int pos, boolean isLongClick);
-}
